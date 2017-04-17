2e14137c4e50d95a33ab3dd91b26cd92544a7fb6
Alexei Pastuchov <alexei.pastuchov@telecolumbus.de>
2017-04-17 12:30:47 +0200

    Client submit job refactoring
    use withGearman because it provides runStateT
    apply hlint suggestions

--- demos/submit-job.hs
+++ demos/submit-job.hs
@@ -1,10 +1,9 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 import Control.Exception (Exception, IOException, catch, throwIO)
-import qualified Control.Monad.State as S
 import qualified Data.ByteString.Char8 as B
 import Data.Typeable     (Typeable)
 import qualified Network.Gearman.Client as C
-import Network.Gearman.Internal (Function, GearmanClient, Port)
+import Network.Gearman.Internal (Function, GearmanClient, GearmanError, Port, withGearman)
 import Network.Socket (HostName)
 
 data ConnectException = ConnectException HostName Port IOException
@@ -13,22 +12,13 @@ instance Exception ConnectException
 
 main :: IO ()
 main = do
-    tryConn `catch` \e -> throwIO (ConnectException host port e)
-    where tryConn = connect >>= either (\e -> putStrLn $ B.unpack e) (\c -> sendJob c >>= \v -> putStrLn v)
-          connect = C.connectGearman (B.pack "i") host port
-          host = "localhost"::HostName
-          port =  4730::Port
-
--- sendJob :: GearmanClient -> IO Bool
-sendJob c = do
-  (res, _) <- flip S.runStateT c $ do
-    res' <- C.submitJob func arg
-    case res' of
-      Left _ -> undefined
-      Right res'' -> do
-        return (B.unpack res'')
-        -- return True
-  return res
-  where func = (B.pack "foo")::Function
-        arg = B.pack "bar"
-
+  c <- C.connectGearman (B.pack "client-id") host port `catch` \e -> throwIO (ConnectException host port e)
+  either (error . B.unpack) return c
+    >>= submitFooJob
+    >>= either(error . B.unpack) (putStrLn . B.unpack)
+  return ()
+    where
+      host = "localhost"::HostName
+      port =  4730::Port
+      submitFooJob :: GearmanClient -> IO (Either GearmanError B.ByteString)
+      submitFooJob gc = withGearman gc $ C.submitJob (B.pack "foo"::Function) (B.pack "bar")
